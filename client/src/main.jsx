aacaf2422be78dd1d718c2c01bec102ffe3830a2
Sanchika Rao <sanchikarao@gmail.com>
2026-07-23 22:31:36 +0530

    fix: update google oauth client id and auth integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { ExpenseProvider } from './context/ExpenseContext';
 import './index.css';
 
 // Paste your Client ID string copied from the downloaded JSON file
-const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID_HERE.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "553063307824-nc0qfkqp7qj4tvkssqh0mp2qdnd1sgb4.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
